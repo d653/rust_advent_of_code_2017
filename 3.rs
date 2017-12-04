57acab8b4503ea23dc0e30d371c09fe736f0c2e8
D <1157896+d653@users.noreply.github.com>
2017-12-04 16:39:35 +0100

    Update 3.rs

--- 3.rs
+++ 3.rs
@@ -3,4 +3,9 @@ fn main() {
     let s = ((n as f64).sqrt() as i32 -1)/2;
     let r = ((n-(2*s+1)*(2*s+1))%(2*s+2) -s -1).abs();
     println!("{}",s+1+r);
+    
+    // too lazy to do the second part in Rust
+    // it can be solved with this one line bash script
+    // curl -s http://oeis.org/A141481/b141481.txt | awk '{ if( int($2) > 361527 ){print $2; exit(0);} }'
+    
 }
