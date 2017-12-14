b57cdc1dea1369769a470498088d0e56e6db33ad
D <1157896+d653@users.noreply.github.com>
2017-12-14 20:37:13 +0100

    Update 14.rs

--- 14.rs
+++ 14.rs
@@ -3,8 +3,11 @@ extern crate itertools;
 extern crate bit_vec;
 use itertools::Itertools;
 use bit_vec::BitVec;
+extern crate petgraph;
+use petgraph::graph::Graph;
+use petgraph::Undirected;
 
-fn hash(s:&str, ret : &mut Vec<u8>) {
+fn hash(s:&str) -> Vec<u8>{
     //copy of advent of code 2017 number 10
     //we add the bytes to ret instead of returning a string
 
@@ -28,45 +31,30 @@ fn hash(s:&str, ret : &mut Vec<u8>) {
     let v3 = (0..64).flat_map(|_|s.chars().map(|c|c as i32)
         .chain(v2.iter().cloned()))
         .collect::<Vec<i32>>();
-    ret.extend((0..256).map(|i|solve(i,&v3))
+    (0..256).map(|i|solve(i,&v3))
         .chunks(16).into_iter()
         .map(|c|c.fold(0,|sum,x|sum^x))
-        .map(|x|x as u8));
+        .map(|x|x as u8).collect()
 }
 
 fn main(){
-    let mut v = Vec::<u8>::new();
-    (0..128).for_each(|i|hash(&format!("hwlqcszp-{}",i),&mut v));
-    
-    let mut b = BitVec::from_bytes(&v);
-    let r1 = b.iter().filter(|x| *x).count();
-    println!("{}",r1);
+    let v : Vec<Vec<u8>> = (0..128).map(|i|hash(&format!("hwlqcszp-{}",i))).collect();
     
     
-    //copy of advent of code 2017 number 12
-    //the list of neighbors is computed differently
-    b.negate();
-    let mut v = Vec::new();
-    let mut p = 0;
-    let mut count = 0;
+    let is_one = |i:i32,j:i32|{ i>=0&&j>=0&&i<128&&j<128&& ((v[i as usize][(j/8) as usize]>>(7-j%8))&1) != 0};
     
-    'outer: loop{
-        while b[p] { p+=1; if p >= b.len() {break 'outer; }}
-        v.push(p);
-        while let Some(x) = v.pop() {
-            if !b[x] {
-                b.set(x,true);
-                let x = x as i32;
-                [(0i32,1i32),(1,0),(-1,0),(0,-1)].iter().map(|&(a,b)|{(x/128+a,x%128 + b)})
-                .filter(|&(a,b)|(0..128).contains(a)&&(0..128).contains(b))
-                .map(|(a,b)|(128*a+b) as usize).filter(|&n|!b[n])
-                .for_each(|n|v.push(n));
-            }
-        }
-        count += 1;
-    }
+    let mut g = Graph::<(), (),Undirected>::from_edges(
+        (0i32..128).cartesian_product(0i32..128).filter(|&(x,y)|is_one(x,y))
+        .flat_map(|(x,y)|{
+            [(0i32,1i32),(1,0),(-1,0),(0,-1)].iter()
+            .map(move |&(i,j)|(x+i,y+j)).filter(|&(a,b)|is_one(a,b))
+            .map(move |(a,b)|((128*x+y) as u32,(128*a+b) as u32))
+        }));
+        
+    g.retain_nodes(|_,idx|{let i = idx.index();is_one((i/128) as i32,(i%128) as i32)});
     
-    println!("{}",count);
+    let r1 = v.iter().map(|x|x.iter().map(|x|x.count_ones()).sum::<u32>()).sum::<u32>();
+    let r2 = petgraph::algo::connected_components(&g);
+    println!("{} {}",r1,r2);
     
 }
-
